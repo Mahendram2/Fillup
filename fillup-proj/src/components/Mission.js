3f172e09d30c3f2f7153513051a8bf714f92f85d
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-11-18 12:24:48 -0600

    foundation of the app

--- fillup-proj/src/components/Mission.js
+++ fillup-proj/src/components/Mission.js
@@ -4,6 +4,8 @@ const Mission = () => {
   return (
     <div>
         <h1>Mission</h1>
+        <img src="https://www.boardeffect.com/wp-content/uploads/2018/10/The-Role-of-the-Nonprofit-Board-in-Building-a-Strong-Mission-Statement.jpg"></img>
+        <p>your mission goes here</p>
     </div>
   )
 }
