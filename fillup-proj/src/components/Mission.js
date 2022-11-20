3317dabd5c750986f0da2a5756ffb55e1dc5eaaa
Mahendra Mishra <110678733+Mahendram2@users.noreply.github.com>
2022-11-20 14:52:56 -0500

    Update Mission.js

--- fillup-proj/src/components/Mission.js
+++ fillup-proj/src/components/Mission.js
@@ -1,27 +1,15 @@
 import React from 'react'
-<<<<<<< HEAD
-import './Mission.css';
 
-=======
 import './Mission.css'
->>>>>>> origin/main
 const Mission = () => {
-    const src = 'https://youtu.be/EUn1on_8xL0';
   return (
     <div className='mission-container'>
-<<<<<<< HEAD
-        <h1>Today's news</h1>
-        <video src={src}>
-
-        </video>
-        <p>your mission goes here</p>
-=======
+        <div></div>
         <h1>Today's News</h1>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/6yCAPAqXodc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <h2>Your mission</h2>
         <h3 className='today-text'>Today, the village is low on clean water, please collect <span>20 liters of clean water</span> to save the village.</h3>
         <h3>We need your help!</h3>
->>>>>>> origin/main
     </div>
   )
 }
