1267001a7b6022f7f6b97884ec329d25b6930b14
James Onwordi <108689956+JamesOnwordi@users.noreply.github.com>
2022-11-20 14:27:35 -0600

    mission.js updated

--- fillup-proj/src/components/Mission.js
+++ fillup-proj/src/components/Mission.js
@@ -4,12 +4,19 @@ import './Mission.css'
 const Mission = () => {
   return (
     <div className='mission-container'>
-        <div></div>
-        <h1>Today's News</h1>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/6yCAPAqXodc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <h2>Your mission</h2>
-        <h3 className='today-text'>Today, the village is low on clean water, please collect <span>20 liters of clean water</span> to save the village.</h3>
-        <h3>We need your help!</h3>
+        <div className='flex justify-center mb-6'>
+        <h1 >Today's News</h1>
+        </div>
+        <div className='flex justify-center mb-6'>
+          <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/6yCAPAqXodc" title="YouTube video player" frameBorder="0"  allowfullscreen></iframe>
+          </div>
+        </div>
+        <div className='flex justify-center mb-6'> 
+          <h2>Your mission</h2>
+        </div>
+        <h3 className='flex justify-center'>Today, the village is low on clean water, please collect  20 liters of clean water to save the village.</h3>
+        <h3 className='flex justify-center'>We need your help!</h3>
     </div>
   )
 }
