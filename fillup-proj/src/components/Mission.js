2097e2d0f81722a87ebd6b442262308552d69926
Mahendra Mishra <110678733+Mahendram2@users.noreply.github.com>
2022-11-21 10:59:08 -0500

    All Good

--- fillup-proj/src/components/Mission.js
+++ fillup-proj/src/components/Mission.js
@@ -1,31 +1,29 @@
 import React from 'react'
+import { FaRegNewspaper } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+// **MUST** - npm i react-icons
 
-// import './Mission.css'
+import './Mission.css'
 const Mission = () => {
   return (
-    <div className='bg-[#c1d9fb] m-12 '>
-      <div className=' mt-6 mb-6'>
-        <div>
-          <div className='flex justify-center mb-6 mt-6'>
-        <br/>
-        <div>
-        <h1 className='font-Raleway'>Today's News</h1>
+    <div className='mission-container'>
+        <div className='flex justify-center mb-6'>
+          <div className="class">
+          <FaRegNewspaper />
+
+          </div>
+        <h1 > Today's News</h1>
         </div>
-        </div>
-        <div className='flex justify-center mb-6 '>
+        <div className='flex justify-center mb-6'>
           <div>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/6yCAPAqXodc" title="YouTube video player" frameBorder="0"  allowfullscreen></iframe>
           </div>
         </div>
         <div className='flex justify-center mb-6'> 
-          <h2 className='font-Raleway'>Your mission</h2>
+          <h2>Your mission</h2>
         </div>
-        <h3 className='flex justify-center font-Raleway'>
-          <div>
-          Today, the village is low on clean water, please collect   <strong className='git font-Raleway'>  20 liters of clean water </strong>to save the village.
-          </div></h3>
-        <h3 className='flex justify-center font-Raleway'>We need your help!</h3>
+        <h3 className='flex justify-center'>Today, the village is low on clean water, please collect <strong> 20 liters of clean water </strong>   to save the village.</h3>
+        <h3 className='flex justify-center'>We need your help!</h3>
         <div className='flex justify-center mb-8'>
         <Link to="/Game"> 
         <button class="bg-transparent flex  hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 mt-7 border border-blue-500 hover:border-transparent rounded">
@@ -34,14 +32,6 @@ const Mission = () => {
         </Link>
         <br/>
         </div>
-        </div>
-      
-        
-        
-        
-      </div>
-  
-        
     </div>
   )
 }
